30d206fb0c6f35e4fb679860ec2bfbdda12afd17
Brendon <torre.brendon@gmail.com>
2018-02-13 22:54:01 +0100

    clean examples

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import Exemple from './Exemple';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Exemple />
+      <div>
       </div>
     );
   }
