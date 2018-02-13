ff9754388e5ff1bd313ef022affa90a371fbd175
Brendon <torre.brendon@gmail.com>
2018-02-13 22:45:39 +0100

    Exemple 2

---
+++ src/Exemple.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Exemple = () => {
+  return (
+    <div className="tb">
+      <table>
+        <tr>
+          <th className='date'></th>
+          <th className='diplome'></th>
+        </tr>
+        <tr>
+          <td className='lieu' colspan='2'></td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default Exemple;
