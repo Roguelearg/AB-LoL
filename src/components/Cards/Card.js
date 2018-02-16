b4656eb2a33f336d2e91492cce7328534bdfac12
Brendon <torre.brendon@gmail.com>
2018-02-16 17:17:59 +0100

    Change border for each card to be thinner

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -4,7 +4,7 @@ import './Card.css';
 const Card = () => {
   return (
     <div className="test">
-      <img alt='A Changer'  src={`http://ddragon.leagueoflegends.com/cdn/8.3.1/img/champion/Warwick.png`} />
+      <img className="charImage" alt='A Changer'  src={`http://ddragon.leagueoflegends.com/cdn/8.3.1/img/champion/Warwick.png`} />
       <h3 className="charname cw">Warwick</h3>
       <p className="winrates cw">53%</p>
     </div>
