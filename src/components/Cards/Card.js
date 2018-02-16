888888bc35fe05bf3cf25b13c9614cedde8b293f
Brendon <torre.brendon@gmail.com>
2018-02-16 17:06:17 +0100

    Add winrate to card

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -6,6 +6,7 @@ const Card = () => {
     <div className="test">
       <img alt='A Changer'  src={`http://ddragon.leagueoflegends.com/cdn/8.3.1/img/champion/Warwick.png`} />
       <h3 className="charname cw">Warwick</h3>
+      <p className="winrates cw">53%</p>
     </div>
   );
 }
