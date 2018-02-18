0a2a3e6e35fc00e36b07b9548de34dcaf719ff6c
Brendon <torre.brendon@gmail.com>
2018-02-18 15:33:57 +0100

    Update css

--- src/App.js
+++ src/App.js
@@ -4,11 +4,28 @@ import WinratesLayout from './containers/WinratesLayout/WinratesLayout';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+      super();
+      this.state = {
+        header: ''
+      };
+  }
+
+  scrollEventListener = () => {
+    window.scrollY > 0 ? this.setState({header: 'active'}) : this.setState({header: ''})
+  }
+
+   componentDidMount() {
+     window.addEventListener('scroll', this.scrollEventListener);
+  }
+
   render() {
+    // console.log('test', this.state.header);
     return (
       <div className="fullContent">
         <div className="contenu">
-          <header className="">
+          <header className={`${this.state.header}`}>
             <h1 className="title cw ft pl2">ARAM Build</h1>
             <nav className="menu ff">
               <a className="cw" href=''>Home</a>
