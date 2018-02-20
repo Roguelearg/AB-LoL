f8fe4e793b4f9cdba9e3040102a2f227fedf9138
Brendon <torre.brendon@gmail.com>
2018-02-20 20:52:34 +0100

    Add a test fetch

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Card from './Card';
 import './Cards.css';
 
+const test = () => {
+  fetch('http://localhost:3000/ab-lol-php/index.php')
+    .then(response => response.json())
+    .then(console.log);
+}
+
 const Cards = () => {
   return (
     <div className="cards">
