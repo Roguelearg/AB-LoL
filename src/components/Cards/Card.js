706d661fb78d0d1adcbf7a7b3f188b06287acb7a
Brendon <torre.brendon@gmail.com>
2018-02-21 22:18:07 +0100

    Add champions recuperation from riot static data

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Card.css';
 
-const Card = () => {
+const Card = ({name, id}) => {
   return (
     <div className="test">
-      <img className="charImage" alt='A Changer'  src={`http://ddragon.leagueoflegends.com/cdn/8.3.1/img/champion/Warwick.png`} />
-      <h3 className="charname">Warwick</h3>
+      <img className="charImage" alt='A Changer'  src={`http://ddragon.leagueoflegends.com/cdn/8.3.1/img/champion/${id}.png`} />
+      <h3 className="charname">{name}</h3>
       <p className="winrates">53%</p>
     </div>
   );
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,88 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Card from './Card';
 import './Cards.css';
 
-const test = () => {
-  fetch('http://localhost:3000/ab-lol-php/index.php')
-    .then(response => response.json())
-    .then(console.log);
-}
+// const test = () => {
+//   fetch('http://localhost:3000/ab-lol-php/index.php')
+//     .then(response => response.json())
+//     .then(console.log);
+//     console.log('test');
+// }
+class Cards extends Component {
 
-const Cards = () => {
-  return (
-    <div className="cards">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  );
+  constructor() {
+    super();
+    this.state = {
+      champions: []
+    }
+  }
+
+  componentDidMount() {
+    this.getNames();
+  }
+
+  getNames = () => {
+    fetch('http://ddragon.leagueoflegends.com/cdn/8.3.1/data/en_US/champion.json')
+      .then(response => response.json())
+      .then(data => {
+        return this.setState({champions: Object.values(data.data)})
+      });
+  }
+
+  makeCards = () => {
+    let vue = [];
+    this.state.champions.forEach(champion => {
+      vue.push(<Card name={champion.name} id={champion.id}/>);
+    })
+    return vue;
+  }
+
+
+  render() {
+    return (
+      <div className="cards">
+        {
+        this.makeCards().map(card => {
+          return card;
+        })
+      }
+      </div>
+    );
+  }
 }
 
 export default Cards;
